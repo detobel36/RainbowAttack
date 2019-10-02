9e672ffd66e871859919d728646feb80a8115e0f
Detobel36 <detobel36@phoenix-rebirth.fr>
2019-10-02 15:00:24 +0200

    Finish sort system (first version)

--- src/rainbow-table.cpp
+++ src/rainbow-table.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
 #include "passwd-utils.hpp"
 #include "reverse.cpp"
 
@@ -13,16 +14,19 @@ const int NBR_PASS = 20;
 // Warning: begin at 1 and end include "NBR_LOOP" (so [1, NBR_LOOP])
 const int NBR_LOOP = 3;
 // Display some debug message
-const bool DEBUG = true;
+const int DEBUG_LEVEL = 1; // 0 = no debug, 1 = some message, 2 = all message
 // Max memore size used to store rainbow row
-const int MEMORY_SIZE = 28; // 1 000 000 000 char per file
+const int MEMORY_SIZE = 9*((PASS_SIZE+1)*2); // Store 50 password per file
+// Note, to have 1Go -> 1 000 000 000 char per file
 
-// struct Rainbow_row {
-//     char pass_head[PASS_SIZE+1];
-//     char pass_tail[PASS_SIZE+1];
-// };
+int sort_array (const void *a, const void *b) {
+    // TODO optimise here
 
-bool sort_array (char i[2][PASS_SIZE+1], char j[2][PASS_SIZE+1]) { return (i[1] < j[1]); }
+    // Get the first element and must sort based on the second
+    char * new_a = ((char *) a) + PASS_SIZE + 1;
+    char * new_b = ((char *) b) + PASS_SIZE + 1;
+    return strcmp(new_a, new_b);
+}
 
 
 // Used for debug
@@ -30,7 +34,7 @@ void read_all_table(std::string file_name) { // display data from file:
     std::ifstream table_file(file_name, std::ios::out | std::ios::binary);
 
     std::cout << std::endl;
-    std::cout << "Content of Rainbow table:" << std::endl;
+    std::cout << "Read file " << file_name << ":" << std::endl;
     if (table_file) {
         std::cout << "Pass size: " << PASS_SIZE << std::endl;
 
@@ -80,10 +84,40 @@ void read_all_table(std::string file_name) { // display data from file:
 //     return NULL;
 // }
 
+void store_batch_in_file(char batch_passwords[][2][PASS_SIZE+1], 
+        int nbr_password_in_batch, std::string output_file, int total_batch) {
+
+    // Sort here
+    qsort(batch_passwords, nbr_password_in_batch, 2*(PASS_SIZE+1)*sizeof(char), sort_array);
+
+    // Create work file
+    std::ofstream table_file(output_file + std::to_string(total_batch) + ".txt", std::ios::out | std::ios::binary);
+    // Write to file
+    table_file.write((char *) batch_passwords, nbr_password_in_batch*2*(PASS_SIZE+1)*sizeof(char));
+    // Close file
+    table_file.close();
+}
+
+// TODO optimise here.  Maybe sort directly when password are fetch (with a heap)
+// Method to have min element index in a sequence...
+int index_min_element(char first_passwords[][2][PASS_SIZE+1], int total_password) {
+    std::size_t index = 0;
+    int min_index = -1;
+
+    while (index < total_password) {
+        if (first_passwords[index][1][0] != '\0' and (min_index < 0 or 
+            strcmp(first_passwords[index][1], first_passwords[min_index][1]) < 0)) {
+            min_index = index;
+        }
+        ++index;
+    }
+    return min_index;
+}
+
 void generate_table(std::string output_file) {
 
     int max_batch_elements = MEMORY_SIZE/(2*(PASS_SIZE+1));
-    if (DEBUG) {
+    if (DEBUG_LEVEL > 0) {
         std::cout << "Max number of element in a batch: " << max_batch_elements << std::endl;
     }
     char batch_passwords[max_batch_elements][2][PASS_SIZE+1];
@@ -94,7 +128,7 @@ void generate_table(std::string output_file) {
     while (i < NBR_PASS) {
         // Generate password
         std::string generate_password = rainbow::generate_passwd(PASS_SIZE);
-        if (DEBUG) {
+        if (DEBUG_LEVEL > 0) {
             std::cout << "Genrated password: " << generate_password << std::endl;
         }
 
@@ -102,12 +136,12 @@ void generate_table(std::string output_file) {
         std::string computed_pass_tail = generate_password;
         for (int i = 1; i <= NBR_LOOP; ++i) {
             computed_pass_tail = reverse(i, sha256(computed_pass_tail), PASS_SIZE);
-            if (DEBUG and i != NBR_LOOP) {
+            if (DEBUG_LEVEL > 1 and i != NBR_LOOP) {
                 std::cout << "Computed intermediary password: " << computed_pass_tail << std::endl;
             }
         }
 
-        if (DEBUG) {
+        if (DEBUG_LEVEL > 0) {
             std::cout << "Computed tail password: " << computed_pass_tail << std::endl;
             std::cout << std::endl; // Add space
         }
@@ -118,15 +152,7 @@ void generate_table(std::string output_file) {
         ++current_batch;
 
         if (current_batch >= max_batch_elements) {
-            // Sort here
-            std::sort(&batch_passwords[0], &batch_passwords[current_batch-1], sort_array);
-
-            // Create work file
-            std::ofstream table_file(output_file + std::to_string(total_batch) + ".txt", std::ios::out | std::ios::binary);
-            // Write to file
-            table_file.write((char *) &batch_passwords, current_batch*2*(PASS_SIZE+1)*sizeof(char));
-            // Close file
-            table_file.close();
+            store_batch_in_file(batch_passwords, current_batch, output_file, total_batch);
             // Reset batch information
             ++total_batch;
             current_batch = 0;
@@ -135,23 +161,77 @@ void generate_table(std::string output_file) {
         ++i;
     }
 
-    // TODO do global sort
+    // Save last batch
+    if (current_batch > 0) {
+        store_batch_in_file(batch_passwords, current_batch, output_file, total_batch);
+        ++total_batch;
+    }
 
+    if (DEBUG_LEVEL > 1) {
+        std::cout << " === Read all files === " << std::endl;
+        for (int i = 0; i < total_batch; ++i) {
+            read_all_table(output_file + std::to_string(i) + ".txt");
+        }
+        std::cout << " ====================== " << std::endl;
+    }
+
+    // Open result file
+    std::ofstream result_table_file(output_file + ".txt", std::ios::out | std::ios::binary);
+    
+    // Open all file and read first pass head and tail
+    char first_passwords[total_batch][2][PASS_SIZE+1];
+    std::ifstream tmp_table_file[total_batch];
+    for (int file_index = 0; file_index < total_batch; ++file_index) {
+        tmp_table_file[file_index] = std::ifstream(output_file + std::to_string(file_index) + ".txt", 
+            std::ios::out | std::ios::binary);
+
+        // Head
+        tmp_table_file[file_index].read((char *) &first_passwords[file_index][0], PASS_SIZE+1*sizeof(char));
+        // Tail
+        tmp_table_file[file_index].read((char *) &first_passwords[file_index][1], PASS_SIZE+1*sizeof(char));
+    }
+
+    int index_pass = 0;
+    while (index_pass < NBR_PASS) {
+        int min_element = index_min_element(first_passwords, total_batch);
+
+        result_table_file.write((char *) first_passwords[min_element], 2*(PASS_SIZE+1)*sizeof(char));
+
+        // Head
+        tmp_table_file[min_element].read((char *) &first_passwords[min_element][0], PASS_SIZE+1*sizeof(char));
+        // Tail
+        bool result = (bool) tmp_table_file[min_element].read((char *) &first_passwords[min_element][1], PASS_SIZE+1*sizeof(char));
+
+        // If we couldn't read the next element, file is finish
+        if (!result) {
+            first_passwords[min_element][0][0] = '\0';
+            first_passwords[min_element][1][0] = '\0';
+        }
+        ++index_pass;
+    }
+
+    for (int file_index = 0; file_index < total_batch; ++file_index) {
+        // Close file
+        tmp_table_file[file_index].close();
+        // Delete file
+        std::string file_name = output_file + std::to_string(file_index) + ".txt";
+        remove(file_name.c_str());
+    }
+    result_table_file.close();
 }
 
 
 int main(int argc, char *argv[]) {
     std::string file_name = "result";
-    std::cout << "fichier: " << file_name << std::endl;
 
     generate_table(file_name);
-    read_all_table(file_name + std::to_string(0) + ".txt");
+    read_all_table(file_name + ".txt");
 
-    reverse(1, "tEsaxa", PASS_SIZE);
-    reverse(1, "tsxaxa", PASS_SIZE);
-    reverse(1, "Oa2axa", PASS_SIZE);
-    reverse(1, "wEsaxa", PASS_SIZE);
-    reverse(1, "tZEaxa", PASS_SIZE);
-    reverse(1, "Oa2axa", PASS_SIZE);
-    reverse(1, "wEsaxa", PASS_SIZE);
+    // reverse(1, "tEsaxa", PASS_SIZE);
+    // reverse(1, "tsxaxa", PASS_SIZE);
+    // reverse(1, "Oa2axa", PASS_SIZE);
+    // reverse(1, "wEsaxa", PASS_SIZE);
+    // reverse(1, "tZEaxa", PASS_SIZE);
+    // reverse(1, "Oa2axa", PASS_SIZE);
+    // reverse(1, "wEsaxa", PASS_SIZE);
 }
